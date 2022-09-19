ef988ebacece9d676c9687896a44981f9217cd7a
neto schmitt <agneloschmittneto@gmail.com>
2022-09-19 18:37:58 -0300

    Envio de E-mail usando protocolo SMTP - Interface/classe Email, appsettings.Json, metodo post EnviarLinkRedefinirSenha implementaçao

--- ControleDeContatos/Controllers/LoginController.cs
+++ ControleDeContatos/Controllers/LoginController.cs
@@ -10,11 +10,14 @@ namespace ControleDeContatos.Controllers
     {
         private readonly IUsuarioRepositorio _usuarioRepositorio;
         private readonly ISessao _sessao;
+        private readonly IEmail _email;
         public LoginController(IUsuarioRepositorio usuarioRepositorio,
-                                ISessao sessao)
+                                ISessao sessao,
+                                IEmail email)
         {
             _usuarioRepositorio = usuarioRepositorio;
             _sessao = sessao;
+            _email=email;
         }
 
         public IActionResult Index()
@@ -84,9 +87,19 @@ namespace ControleDeContatos.Controllers
                     if(usuario != null)
                     {
                         string novaSenha = usuario.GerarNovaSenha();
-                        _usuarioRepositorio.Atualizar(usuario);
+                        string mensagem = $"Sua nova senha é: {novaSenha}";
 
-                        TempData["MensagemSucesso"] = $" Enviamos para o seu Email cadastrado uma nova senha.";
+                        bool emailEnviado = _email.Enviar(usuario.Email, "Sistema de Contatos - Nova Senha", mensagem);
+
+                        if (emailEnviado)
+                        {
+                            _usuarioRepositorio.Atualizar(usuario);
+                            TempData["MensagemSucesso"] = $" Enviamos para o seu Email cadastrado uma nova senha.";
+                        }
+                        else
+                        {
+                            TempData["MensagemErro"] = $"Não conseguimos enviar o email. Verifique os dados informados e tente novamente!";
+                        }
                         return RedirectToAction("Index", "Login");
                     }
 
